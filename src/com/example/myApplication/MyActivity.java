c8deac7df07f0157251b769f430fdd47c8e4ae1e
Adama-S <adama_sakho@hotmail.com>
2015-05-31 21:15:35 +0200

    add  list of application (name + icon)

--- src/com/example/myApplication/MyActivity.java
+++ src/com/example/myApplication/MyActivity.java
@@ -5,22 +5,23 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.graphics.drawable.*;
+import android.content.pm.ResolveInfo;
 import android.os.BatteryManager;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
-
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class MyActivity extends Activity {
     private TextView batteryInfo;
     private ImageView imageBatteryState;
+
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,10 +30,22 @@ public class MyActivity extends Activity {
         batteryInfo=(TextView)findViewById(R.id.textViewBatteryInfo);
         imageBatteryState=(ImageView)findViewById(R.id.imageViewBatteryState);
 
-        this.registerReceiver(this.batteryInfoReceiver,	new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        loadApps();
+
+        this.registerReceiver(this.pluggedReceiver,	new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
     }
 
-    private BroadcastReceiver batteryInfoReceiver = new BroadcastReceiver() {
+    private void loadApps() {
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        List<ResolveInfo> mApps = getPackageManager().queryIntentActivities(mainIntent, 0);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(new AppsAdapter(this,mApps));
+    }
+
+    public BroadcastReceiver pluggedReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
 
@@ -46,40 +59,71 @@ public class MyActivity extends Activity {
             String  technology= intent.getExtras().getString(BatteryManager.EXTRA_TECHNOLOGY);
             int  temperature= intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0);
             int  voltage= intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0);
-            PackageManager appInfo = getPackageManager();
-            List<ApplicationInfo> list = appInfo.getInstalledApplications(0);
-
-            Drawable icon ;
-            Drawable logo ;
-            CharSequence label = "" ;
-            ArrayList<CharSequence> lstChrS = new ArrayList<CharSequence>();
-            ArrayList<Drawable> lstDraw = new ArrayList<Drawable>();
-            for(ApplicationInfo applicationInfo : list){
-                icon = getPackageManager().getApplicationIcon(applicationInfo);
-                label = getPackageManager().getApplicationLabel(applicationInfo);
-                logo = getPackageManager().getApplicationLogo(applicationInfo);
-                lstDraw.add(icon);
-                lstDraw.add(logo);
-                lstChrS.add(label);
-            }
-
 
             batteryInfo.setText(
-//                    "Health: "+health+"\n"+
+                            "Health: "+health+"\n"+
                             "Level: "+level+"\n"+
                             "Plugged: "+plugged+"\n"+
                             "Present: "+present+"\n"+
                             "Voltage: "+voltage+"\n"+
-//                            "Scale: "+scale+"\n"+
-//                            "Status: "+status+"\n"+
+                            "Scale: "+scale+"\n"+
+                            "Status: "+status+"\n"+
                             "Technology: "+technology+"\n"+
-                            "Temperature: "+temperature+"\n"+
-                            "Label: "+lstChrS+"\n"+
-                            "Drawlable: "+lstDraw+"\n");
+                            "Temperature: "+temperature+"\n"
 
+            );
 
             imageBatteryState.setImageResource(icon_small);
         }
     };
+
+    public class AppsAdapter extends BaseAdapter {
+        private LayoutInflater inflater;
+        private List<ResolveInfo> mApps;
+
+        public AppsAdapter(Context context, List<ResolveInfo> mApps) {
+            this.inflater = LayoutInflater.from(context);
+            this.mApps = mApps;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHendler hendler;
+            if (convertView == null) {
+                convertView = inflater.inflate(R.layout.list_application, null);
+                hendler = new ViewHendler();
+                hendler.textLable = (TextView)convertView.findViewById(R.id.label);
+                hendler.iconImage = (ImageView)convertView.findViewById(R.id.imageView);
+                convertView.setTag(hendler);
+            } else {
+                hendler = (ViewHendler)convertView.getTag();
+            }
+            ResolveInfo info = this.mApps.get(position);
+            hendler.iconImage.setImageDrawable(info.loadIcon(getPackageManager()));
+            hendler.textLable.setText(info.loadLabel(getPackageManager()));
+
+            return convertView;
+
+        }
+        class ViewHendler{
+            TextView textLable;
+            ImageView iconImage;
+        }
+
+
+        public final int getCount() {
+            return mApps.size();
+        }
+
+        public final Object getItem(int position) {
+            return mApps.get(position);
+        }
+
+        public final long getItemId(int position) {
+            return position;
+        }
+    }
+
+
 
 }
